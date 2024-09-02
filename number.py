4d3efff9e223b088ca3187ad5dee4bd78252d3e2
Frank Mwito <Mfranklyne039@gmail.com>
2024-09-02 18:15:37 +0300

    match-case to print if the number is positive, negative, or zero

--- number.py
+++ number.py
@@ -1,1 +1,14 @@
 # match-case to print if the number is positive, negative, or zero.
+
+def number_match(number):
+    match number:
+        case int() if number > 0: # can use wildcard case _ if number > 0:
+            return "positive"
+        case int() if  number < 0:
+            return "negative"
+        case int() if number == 0:
+            return "zero"
+        case _:
+            return "Invalid input"
+        
+print(number_match(int(input("Enter any number: "))))
