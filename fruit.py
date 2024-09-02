b32c8caca7080ed3b7f50f737a168f7e51ee36b5
Frank Mwito <Mfranklyne039@gmail.com>
2024-09-02 18:01:04 +0300

    match case of a friut with message

--- fruit.py
+++ fruit.py
@@ -1,2 +1,19 @@
 # match case of a friut with message
 
+def friut_message(fruit):
+    ruit = fruit.lower()
+    match ruit:
+        case "apple":
+            return "Apples are sweet"
+        case "banana":
+            return "Bananas are yellow"
+        case "tangerine":
+            return "Tangerines are sour"
+        case "ovacado":
+            return "ovacados are huge"
+        case "watermelon":
+            return "watermelon are juicy"
+        case _:
+            return "invalid choice of fruit"
+   
+print(friut_message( input("Enter your choice of fruit: ")))
